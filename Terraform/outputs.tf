888d16cfaac2bb4f68095bc5fd942cac3f81cd55
sirhaffy <haffy@icloud.com>
2025-09-24 08:45:44 +0200

    fix - Trying to get SSH test to work in CI/CD

--- Terraform/outputs.tf
+++ Terraform/outputs.tf
@@ -75,4 +75,4 @@ output "worker2_private_ip" {
   value       = length(module.docker_swarm.swarm_worker_private_ips) > 1 ? module.docker_swarm.swarm_worker_private_ips[1] : null
 }
 
-# Note: The individual node IP outputs are primarily for testing and verification purposes.
+# Note: The individual node IP outputs are primarily for testing and verification purposes..
